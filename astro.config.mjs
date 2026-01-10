bee47a330b544afc7982c5a6ffa87b1bf8bf2fe4
Jared Schraub <jaredschraub@outlook.com>
2026-01-10 01:54:57 -0600

    added cloudflare integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [mdx(), sitemap(), react()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  adapter: cloudflare(),
 });
