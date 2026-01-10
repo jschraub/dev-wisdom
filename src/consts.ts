d0cbdcf446954f9744ae5b359e201f3b458e028c
Jared Schraub <jaredschraub@outlook.com>
2026-01-10 02:14:44 -0600

    Updated author name and github

--- src/consts.ts
+++ src/consts.ts
@@ -5,3 +5,5 @@ export const SITE_TITLE = "Dev Wisdom";
 export const SITE_DESCRIPTION =
   "Blog style write-ups of wisdom gathered as a developer and engineer.";
 export const SITE_AUTHOR = "Jared Schraub";
+export const SITE_AUTHOR_SHORT_NAME = "Jared";
+export const SITE_AUTHOR_GITHUB = "https://github.com/jschraub";
