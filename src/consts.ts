135db9b9e47f54df54991fcf73253e8a34e3c792
Jared Schraub <jaredschraub@outlook.com>
2026-01-10 01:40:23 -0600

    updated site consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dev Wisdom";
+export const SITE_DESCRIPTION =
+  "Blog style write-ups of wisdom gathered as a developer and engineer.";
+export const SITE_AUTHOR = "Jared Schraub";
