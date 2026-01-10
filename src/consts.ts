7fe99517720c3c9c01e3691134b98f6fbcc3d7ce
Jared Schraub <jaredschraub@outlook.com>
2026-01-10 02:31:18 -0600

    changed template

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Dev Wisdom";
-export const SITE_DESCRIPTION =
-  "Blog style write-ups of wisdom gathered as a developer and engineer.";
-export const SITE_AUTHOR = "Jared Schraub";
-export const SITE_AUTHOR_SHORT_NAME = "Jared";
-export const SITE_AUTHOR_GITHUB = "https://github.com/jschraub";
